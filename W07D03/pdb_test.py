2a301fa9ffb8d0cfe759066858484680be0be102
fivecube <mohitchouhan1947@gmail.com>
2022-01-19 22:19:56 +0530

    W07D03

---
+++ W07D03/pdb_test.py
@@ -0,0 +1,11 @@
+def sum_of_age(x,y):
+    return x+y
+
+breakpoint()
+a = input('a')
+b = input('b')
+
+print(sum_of_age(a,b))
+# here the bug is it not giving the correct sum of age as 46 when a is 12
+# and b is 34
+
